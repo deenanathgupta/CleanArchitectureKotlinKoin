3bc670af837d62cf6fc2fa17acbba9b5478f5308
deenanath <deena_cont@olacabs.com>
2020-09-22 20:53:24 +0530

    Unit test added

---
+++ app/src/test/java/com/android/cardinalhealthtask/data/repository/AlbumRepositoryImpTest.kt
@@ -0,0 +1,51 @@
+package com.android.cardinalhealthtask.data.repository
+
+import com.android.cardinalhealthtask.domain.model.Album
+import com.android.cardinalhealthtask.domain.model.AlbumItem
+import io.mockk.MockKAnnotations
+import io.mockk.every
+import io.mockk.impl.annotations.MockK
+import io.mockk.mockk
+import kotlinx.coroutines.runBlocking
+import org.hamcrest.CoreMatchers
+import org.hamcrest.MatcherAssert
+import org.junit.Before
+import org.junit.Test
+
+
+class AlbumRepositoryImpTest {
+
+    @MockK
+    lateinit var albumRepository: AlbumRepositoryImp
+
+    @Before
+    fun setUp() {
+        MockKAnnotations.init(this)
+    }
+
+    @Test
+    fun getAlbumData() = runBlocking {
+        val albums = mockk<List<Album>>()
+        every { runBlocking { albumRepository.getAlbums() } } returns (albums)
+
+        val result = albumRepository.getAlbums()
+        MatcherAssert.assertThat(
+            "Album Received result [$result] & mocked [$albums] must be matches on each other!",
+            result,
+            CoreMatchers.`is`(albums)
+        )
+    }
+
+    @Test
+    fun getPhotosData() = runBlocking {
+        val photos = mockk<List<AlbumItem>>()
+        every { runBlocking { albumRepository.getPhotos("1") } } returns (photos)
+
+        val result = albumRepository.getPhotos("1")
+        MatcherAssert.assertThat(
+            "Photo Received result [$result] & mocked [$photos] must be matches on each other!",
+            result,
+            CoreMatchers.`is`(photos)
+        )
+    }
+}
---
+++ app/src/test/java/com/android/cardinalhealthtask/data/repository/KoinModuleTest.kt
@@ -0,0 +1,19 @@
+package com.android.cardinalhealthtask.data.repository
+
+import com.android.cardinalhealthtask.di.module.AlbumModule
+import com.android.cardinalhealthtask.di.module.NetworkModule
+import org.junit.Test
+import org.koin.dsl.koinApplication
+import org.koin.test.AutoCloseKoinTest
+import org.koin.test.check.checkModules
+
+class KoinModuleTest : AutoCloseKoinTest() {
+
+    @Test
+    fun testCoreModule() {
+        koinApplication {
+            modules(listOf(AlbumModule, NetworkModule))
+        }.checkModules()
+    }
+
+}
